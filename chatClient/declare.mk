fe779ee6515137479821459db9769bd6f7060e42
htqvinh <htqvinh@localhost>
2016-12-23 02:06:21 -0800

    update ChatClient

--- chatClient/declare.mk
+++ chatClient/declare.mk
@@ -4,11 +4,13 @@ SOURCEDIR 	= source
 FLAGS 		= -g
 
 INCLUDE 	= -I./include \
-	-I/home/vinhhtq/cpp_examples/vnet/include \
-	-I/home/vinhhtq/cpp_examples/vlog/include \
+	-I/data/proj/cpp_examples/vnet/include \
+	-I/data/proj/cpp_examples/vlog/include \
+	-I/data/proj/cpp_examples/chatGen/include \
 	
 LIB 		= \
-	-L/home/vinhhtq/cpp_examples/vnet -lVnet \
-	-L/home/vinhhtq/cpp_examples/vlog -lVlog \
-	-L/home/vinhhtq/cpp_examples/vutil -lVutil \
+	-L/data/proj/cpp_examples/vnet -lVnet \
+	-L/data/proj/cpp_examples/vlog -lVlog \
+	-L/data/proj/cpp_examples/vutil -lVutil \
+	-L/data/proj/cpp_examples/chatGen -lChatGen \
 #
--- chatClient/source/main.cpp
+++ chatClient/source/main.cpp
@@ -6,12 +6,23 @@
  */
 
 #include <iostream>
-#include "ConnectorTCP.h"
+#include "ChatGeneral.h"
+#include "ConnectorBase.h"
 using namespace std;
 
+#define NUM_CONN 1000
+
 int main (int argc, char** argv) {
+
 	ConnectorTCP tcp;
-	tcp.connect("127.0.0.1", 5001);
+
+	for(int i = 0; i < NUM_CONN; ++i){
+		StreamBase* stream = tcp.connect("127.0.0.1", 5001);
+		string hello = "Hello world!!!";
+		CMessage mess = { MTYPE::MESS_A, hello.size(), hello.c_str()};
+		mess.sendTo(stream);
+	}
+
 	cout << argv[0] << endl;
 	return 0;
 }
