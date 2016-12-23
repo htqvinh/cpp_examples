9f16fbcffe347c09feb3361e3f5d96b5daa1044f
htqvinh <htqvinh@localhost>
2016-12-23 02:08:13 -0800

    update ChatServer

--- chatServer/declare.mk
+++ chatServer/declare.mk
@@ -1,14 +1,14 @@
 # editable
 TARGET 		= ChatServer
 SOURCEDIR 	= source
-FLAGS 		= -g
+FLAGS 		= -g -std=c++11
 
 INCLUDE 	= -I./include \
-	-I/home/vinhhtq/cpp_examples/vnet/include \
-	-I/home/vinhhtq/cpp_examples/vlog/include \
+	-I/data/proj/cpp_examples/vnet/include \
+	-I/data/proj/cpp_examples/vlog/include \
 	
-LIB 		= \
-	-L/home/vinhhtq/cpp_examples/vnet -lVnet \
-	-L/home/vinhhtq/cpp_examples/vlog -lVlog \
-	-L/home/vinhhtq/cpp_examples/vutil -lVutil \
+LIB 		= -lpthread \
+	-L/data/proj/cpp_examples/vnet -lVnet \
+	-L/data/proj/cpp_examples/vlog -lVlog \
+	-L/data/proj/cpp_examples/vutil -lVutil \
 #
--- chatServer/source/main.cpp
+++ chatServer/source/main.cpp
@@ -6,19 +6,27 @@
  */
 
 #include <iostream>
+#include <thread>
 #include "defines.h"
 #include "AcceptorBase.h"
 
 using namespace std;
 
+void listen_func(AcceptorBase* acc){
+	while(acc){
+		StreamBase* stream = acc->accept();
+		cout << MAIN_LOG("has a connect from (%s)\n", stream->getIP().c_str());
+	}
+}
+
 int main (int argc, char** argv) {
+
 	AcceptorTCP acc;
 	if(acc.init(5001)){
-		do{
-			StreamBase* stream = acc.accept();
-			cout << MAIN_LOG("has a connect from (%s)\n", stream->getIP().c_str());
-		}while(true);
+		std::thread t(listen_func, &acc);
+		t.join();
 	}
+
 	return 0;
 }
 
